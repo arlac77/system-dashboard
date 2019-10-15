6b7aad174fc376626eefd39801b12ca3005f28ca
Markus Felten <Markus.Felten@gmx.de>
2019-10-15 07:17:37 -0700

    chore(package): correct repository url chore(package): set bugs url from template chore(package): homepage from template chore(package): set template repo chore(package): add documentation@^12.1.2 chore(package): add markdown-doctest@^1.0.0 chore(package): add ava@^2.4.0 chore(package): add c8@^5.0.4 chore(package): add esm@^3.2.25 chore(package): add rollup@^1.24.0 chore(package): add rollup-plugin-commonjs@^10.1.0 chore(package): add rollup-plugin-cleanup@^3.1.1 chore(package): add rollup-plugin-node-resolve@^5.2.0 chore(package): add rollup-plugin-json@^4.0.0 chore(package): add rollup-plugin-executable@^1.5.1 chore(package): add builtin-modules@^3.1.0 chore(package): add semantic-release@^15.13.26 chore(scripts): add cover@#overwrite c8 -x 'tests/**/*.mjs' --temp-directory build/tmp ava && c8 report -r lcov -o build/coverage --temp-directory build/tmp chore(scripts): add test@ava chore(scripts): add posttest@npm run prepare && markdown-doctest chore(scripts): add prepare@rollup -c chore(scripts): add docs@documentation readme {{module}} --section=API chore(scripts): add lint@documentation lint {{module}} chore(engines): add node@>=12.12.0 chore(package): add version from template chore(package): add description from template chore(package): add module from template chore(package): add main from template chore(package): add publishConfig from template chore(package): add keywords from template chore(package): add dependencies from template chore(package): add contributors from template chore(package): add license from template chore(package): add ava from template docs(package): remove keyword null chore(package): remove unknown value for description ({{description}}) chore(package): remove unknown value for main ({{main}}) chore(package): remove unknown value for module ({{module}}) chore(travis): add node_js (language) chore(travis): (branches) chore(travis): (jobs) chore(travis): (script) chore(rollup): copy from template chore(license): add LICENSE chore(npm): update .npmignore from template chore: add missing README.md from template

---
+++ README.md
@@ -0,0 +1,19 @@
+# system-dashboard
+
+{{description}}
+
+# usage
+
+# API
+
+# install
+
+With [npm](http://npmjs.org) do:
+
+```shell
+npm install system-dashboard
+```
+
+# license
+
+BSD-2-Clause
---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import json from "rollup-plugin-json";
+import cleanup from "rollup-plugin-cleanup";
+import builtins from "builtin-modules";
+import pkg from "./package.json";
+
+const external = [...builtins];
+
+export default [
+  ...Object.keys(pkg.bin || {}).map(name => {
+    return {
+      input: `src/${name}-cli.mjs`,
+      output: {
+        file: pkg.bin[name],
+        format: "cjs",
+        banner:
+          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+        interop: false,
+        externalLiveBindings: false
+      },
+      plugins: [
+        commonjs(),
+        json({
+          include: "package.json",
+          preferConst: true,
+          compact: true
+        }),
+        cleanup({
+          extensions: ["js", "mjs", "jsx", "tag"]
+        }),
+        executable()
+      ],
+      external
+    };
+  }),
+  {
+    input: pkg.module,
+    output: {
+      file: pkg.main,
+      format: "cjs",
+      interop: false,
+      externalLiveBindings: false
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      cleanup({
+        extensions: ["js", "mjs", "jsx", "tag"]
+      })
+    ]
+  }
+];
