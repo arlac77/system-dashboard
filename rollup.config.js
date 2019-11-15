9d753cda7bc34800d19688dff8b5e41b0c2252a2
Markus Felten <Markus.Felten@gmx.de>
2019-11-15 11:55:48 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import builtins from "builtin-modules";
 import pkg from "./package.json";
@@ -12,33 +12,46 @@ const external = [...builtins,
   "koa",
   "koa-better-router"
 ];
+const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+    preferConst: true,
+    compact: true
+  }),
+  cleanup({
+    extensions
+  })
+];
 
-export default 
-  Object.keys(pkg.bin || {}).map(name => {
-    return {
-      input: `src/${name}-cli.mjs`,
-      output: {
-        file: pkg.bin[name],
-        format: "cjs",
-        banner:
-          '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
-        interop: false,
-        externalLiveBindings: false
-      },
-      plugins: [
-        commonjs(),
-        resolve(),
-        json({
-          include: "package.json",
-          preferConst: true,
-          compact: true
-        }),
-        cleanup({
-          extensions: ["js", "mjs", "jsx", "tag"]
-        }),
-        executable()
-      ],
-      external
-    };
+const config = Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
+  };
+});
+
+if (pkg.module !== undefined && pkg.main !== undefined && pkg.module != pkg.main) {
+  config.push({
+    input: pkg.module,
+    output: {
+      file: pkg.main
+    }
   });
+}
 
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output
+  };
+  return { plugins, external, ...c };
+});
