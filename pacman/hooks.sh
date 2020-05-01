eacd2536d82713c58d19351f3d7076938d545dca
Markus Felten <Markus.Felten@gmx.de>
2020-05-01 21:54:29 +0200

    fix(pacman): restart service on update

--- pacman/hooks.sh
+++ pacman/hooks.sh
@@ -3,25 +3,22 @@ post_install() {
 	systemctl daemon-reload
 	systemctl enable {{name}}
 	systemctl enable {{name}}.socket
+	systemctl start {{name}}
 	systemctl start {{name}}.socket
 	systemctl is-enabled nginx 2>&1 >/dev/null && systemctl -q try-reload-or-restart nginx
 }
 
-pre_upgrade() {
-	systemctl stop {{name}}.socket
-	systemctl stop {{name}}
-}
-
 post_upgrade() {
 	systemctl daemon-reload
-	systemctl start {{name}}.socket
+	systemctl restart {{name}}
+	systemctl restart {{name}}.socket
 	systemctl is-enabled nginx 2>&1 >/dev/null && systemctl -q try-reload-or-restart nginx
 }
 
 pre_remove() {
 	systemctl stop {{name}}.socket
-	systemctl disable {{name}}.socket
 	systemctl stop {{name}}
+	systemctl disable {{name}}.socket
 	systemctl disable {{name}}
 }
 
