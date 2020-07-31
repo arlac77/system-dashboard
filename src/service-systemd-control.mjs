8c859f894d8f2ff44f0f02a3d9594bf2248dd4b9
Markus Felten <Markus.Felten@gmx.de>
2020-07-31 12:09:54 +0200

    feat: decode timers & sockets

--- src/service-systemd-control.mjs
+++ src/service-systemd-control.mjs
@@ -12,13 +12,7 @@ export function decodeUnits(data) {
   // backup.service                             loaded    inactive dead    backup
 
   return data.split(/\n/).map(line => {
-    const [
-      unit,
-      load,
-      active,
-      sub,
-      ...description
-    ] = line.split(/\s+/);
+    const [unit, load, active, sub, ...description] = line.split(/\s+/);
     return {
       unit,
       load,
@@ -30,31 +24,52 @@ export function decodeUnits(data) {
 }
 
 export function decodeTimers(data) {
-  // Fri 2020-07-31 00:00:00 CEST 2min 7s left  Thu 2020-07-30 00:00:00 CEST 23h ago    logrotate.timer              logrotate.service             
-  // Fri 2020-07-31 00:00:00 CEST 2min 7s left  Thu 2020-07-30 00:00:00 CEST 23h ago    man-db.timer                 man-db.service                
-  return data;
+  /*
+  NEXT                         LEFT          LAST                         PASSED      UNIT                         ACTIVATES                     
+Fri 2020-07-31 12:10:28 CEST 17min left    Thu 2020-07-30 12:10:28 CEST 23h ago     systemd-tmpfiles-clean.timer systemd-tmpfiles-clean.service
+Fri 2020-07-31 13:50:00 CEST 1h 57min left Fri 2020-07-31 09:50:02 CEST 2h 2min ago desec.timer                  desec.service                 
+  */
+  return data.split(/\n/).map(line => {
+    return {
+      next: line.substr(0, 28),
+      left: line.substr(29, 14).trim(),
+      last: line.substr(43, 28),
+      passed: line.substr(72, 11).trim(),
+      unit: line.substr(83).split(/\s+/)[0],
+      activates: line.substr(83).split(/\s+/)[1]
+    };
+  });
 }
 
 export function decodeSockets(data) {
-  // /run/avahi-daemon/socket                       avahi-daemon.socket                  avahi-daemon.service                 
-  // /run/dbus/system_bus_socket                    dbus.socket                          dbus.service                         
-  return data;
+  /*
+LISTEN                                         UNITS                                ACTIVATES
+/run/avahi-daemon/socket                       avahi-daemon.socket                  avahi-daemon.service                 
+/run/dbus/system_bus_socket                    dbus.socket                          dbus.service                         
+/run/udev/control                              systemd-udevd-control.socket         systemd-udevd.service                
+[::]:19531                                     systemd-journal-gatewayd.socket      systemd-journal-gatewayd.service     
+audit 1                                        systemd-journald-audit.socket        systemd-journald.service             
+kobject-uevent 1                               systemd-udevd-kernel.socket          systemd-udevd.service                
+route 1361                                     systemd-networkd.socket              systemd-networkd.service             
+*/
+  return data.split(/\n/).map(line => {
+    const last = line.substr(47).split(/\s+/);
+    return {
+      listen: line.substring(0,47).trim(),
+      units: last[0],
+      activates: last[1]
+    };
+  });
 }
 
 export function decodeMachines(data) {
-/*
+  /*
   NAME         STATE    FAILED JOBS
 * pine1 (host) degraded 2      0   
 */
 
   return data.split(/\n/).map(line => {
-    const [
-      name,
-      type,
-      state,
-      failed,
-      jobs
-    ] = line.split(/\s+/);
+    const [name, type, state, failed, jobs] = line.split(/\s+/);
     return {
       name,
       state,
--- tests/systemctl-test.mjs
+++ tests/systemctl-test.mjs
@@ -1,5 +1,10 @@
 import test from "ava";
-import { decodeUnits, decodeSockets, decodeTimers, decodeMachines } from "../src/service-systemd-control.mjs";
+import {
+  decodeUnits,
+  decodeSockets,
+  decodeTimers,
+  decodeMachines
+} from "../src/service-systemd-control.mjs";
 
 test("systemctl decode units", t => {
   const raw = `auditd.service                             loaded    inactive dead    Security Auditing Service
@@ -31,15 +36,62 @@ backup.service                             loaded    inactive dead    backup`;
   ]);
 });
 
-test.skip("systemctl decode timers", t => {
+test("systemctl decode timers", t => {
   const raw = `Fri 2020-07-31 00:00:00 CEST 2min 7s left  Thu 2020-07-30 00:00:00 CEST 23h ago    logrotate.timer              logrotate.service             
 Fri 2020-07-31 00:00:00 CEST 2min 7s left  Thu 2020-07-30 00:00:00 CEST 23h ago    man-db.timer                 man-db.service  `;
 
-  t.deepEqual(decodeTimers(raw), [{},{}]);
+  t.deepEqual(decodeTimers(raw), [
+    {
+      next: "Fri 2020-07-31 00:00:00 CEST",
+      left: "2min 7s left",
+      last: "Thu 2020-07-30 00:00:00 CEST",
+      passed: "23h ago",
+      unit: "logrotate.timer",
+      activates: "logrotate.service"
+    },
+    {
+      next: "Fri 2020-07-31 00:00:00 CEST",
+      left: "2min 7s left",
+      last: "Thu 2020-07-30 00:00:00 CEST",
+      passed: "23h ago",
+      unit: "man-db.timer",
+      activates: "man-db.service"
+    }
+  ]);
 });
 
-
-test.skip("systemctl decode machines", t => {
+test("systemctl decode machines", t => {
   const raw = `pine1 (host) degraded 2      0   `;
-  t.deepEqual(decodeMachines(raw), [{name: "pine1", state: "degraded", failed: 2, jobs: 0}]);
+  t.deepEqual(decodeMachines(raw), [
+    { name: "pine1", state: "degraded", failed: 2, jobs: 0 }
+  ]);
+});
+
+test("systemctl decode sockets", t => {
+  const raw = `/run/avahi-daemon/socket                       avahi-daemon.socket                  avahi-daemon.service                 
+/run/dbus/system_bus_socket                    dbus.socket                          dbus.service                         
+[::]:19531                                     systemd-journal-gatewayd.socket      systemd-journal-gatewayd.service     
+route 1361                                     systemd-networkd.socket              systemd-networkd.service             `;
+  t.deepEqual(decodeSockets(raw), [
+    {
+      listen: "/run/avahi-daemon/socket",
+      units: "avahi-daemon.socket",
+      activates: "avahi-daemon.service"
+    },
+    {
+      listen: "/run/dbus/system_bus_socket",
+      units: "dbus.socket",
+      activates: "dbus.service"
+    },
+    {
+      listen: "[::]:19531",
+      units: "systemd-journal-gatewayd.socket",
+      activates: "systemd-journal-gatewayd.service"
+    },
+    {
+      listen: "route 1361",
+      units: "systemd-networkd.socket",
+      activates: "systemd-networkd.service"
+    }
+  ]);
 });
