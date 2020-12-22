d2c9e8d66de1df4db796d56274cd225dbc1cc17d
Markus Felten <Markus.Felten@gmx.de>
2020-12-22 16:02:31 +0100

    test: compare dates not strings

--- tests/util-test.mjs
+++ tests/util-test.mjs
@@ -5,18 +5,18 @@ test("decode date", t => {
   const line =
     "Thu 2020-08-13 00:00:00 CEST 3h 25min left Wed 2020-08-12 00:00:03 CEST 20h ago    logrotate.timer              logrotate.service";
 
-  t.is(
-    decodeDate(line).toString(),
-    "Wed Aug 12 2020 22:00:00 GMT+0200 (Central European Summer Time)"
+  t.deepEqual(decodeDate(line), new Date("Wed Aug 12 2020 22:00:00 GMT+0200"));
+
+  t.deepEqual(
+    decodeDate(line.substring(line.indexOf("left") + 5)),
+    new Date("Tue Aug 11 2020 22:00:03 GMT+0200")
   );
 
-  t.is(
-    decodeDate(line.substring(line.indexOf("left") + 5)).toString(),
-    "Tue Aug 11 2020 22:00:03 GMT+0200 (Central European Summer Time)"
+  t.deepEqual(
+    line
+      .substring(line.indexOf("ago") + 4)
+      .trim()
+      .split(/\s+/),
+    ["logrotate.timer", "logrotate.service"]
   );
-
-  t.deepEqual(line.substring(line.indexOf("ago") + 4).trim().split(/\s+/), [
-    "logrotate.timer",
-    "logrotate.service"
-  ]);
 });
